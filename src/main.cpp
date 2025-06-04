592966e0777fd23bf58010268968ccfc3e1ef72d
Bavin <bavinbav5@gmail.com>
2025-06-04 19:59:45 +0300

    i added 2 new functions which is Digital set with ASM and clear

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <avr/pgmspace.h>
 
 char buff[80];
 volatile uint16_t x=19,y=7;
@@ -11,6 +11,8 @@ uint16_t Add(uint16_t x,uint16_t y);
 uint16_t Sub(uint16_t x,uint16_t y);
 uint16_t Mul(uint16_t x,uint16_t y);
 void Set_LED(uint8_t set);
+void PinModeASMset(uint8_t port,uint8_t bit);
+void PinModeASMclear(uint8_t port,uint8_t bit);
 }
 void setup() {
   
@@ -22,16 +24,18 @@ void setup() {
   sprintf(buff,"%d * %d = %d",x,y,Mul(x,y));
   Serial.println(buff);
   //pinMode(13,OUTPUT);
-
+  PinModeASMset(_SFR_IO_ADDR(DDRB), 1<<PB3);
 }
 
 void loop() {
   // set LED is the same as digitalWrite function but the one i used is pure ASM  :)
   //digitalWrite(13,HIGH);
-  Set_LED(HIGH);
+  //Set_LED(HIGH);
+  PinModeASMset(_SFR_IO_ADDR(PORTB), 1<<PB3);
   delay(100);
   //digitalWrite(13,LOW);
-  Set_LED(LOW);
+  //Set_LED(LOW);
+  PinModeASMclear(_SFR_IO_ADDR(PORTB), 1<<PB3);
   delay(100);
 
 }
